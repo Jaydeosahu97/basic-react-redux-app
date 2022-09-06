13396f68a6978d2c7ade9ec3da8f996028cae295
Jaydeo Sahu <jaydeosahu@gmail.com>
2022-09-06 17:54:32 +0530

    initial commit

---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+function Shop() {
+  return (
+    <div className="position-relative top-0 start-50">
+      <h2>Add to Cart</h2>
+      <button className="btn btn-primary">+</button>
+      Update balance
+      <button className="btn btn-primary">-</button>
+    </div>
+  );
+}
+
+export default Shop;
